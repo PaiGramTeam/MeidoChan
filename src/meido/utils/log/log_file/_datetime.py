daa483d057100d4f902d305ba0f8d2f0632940c5
Karako <karakohear@gmail.com>
2024-01-04 11:39:51 +0800

    :green_heart:

--- src/meido/utils/log/log_file/_datetime.py
+++ src/meido/utils/log/log_file/_datetime.py
@@ -10,7 +10,7 @@ __all__ = (
 )
 
 tokens = r"H{1,2}|h{1,2}|m{1,2}|s{1,2}|S+|YYYY|YY|M{1,4}|D{1,4}|Z{1,2}|zz|A|X|x|E|Q|dddd|ddd|d"
-pattern = re.compile(r"(?:{0})|\[(?:{0}|!UTC|)\]".format(tokens))
+pattern = re.compile(r"(?:{0})|\[(?:{0}|!UTC|)\]".format(tokens))  # skipcq: PYL-C0209
 
 
 class FileDateFormatter:
